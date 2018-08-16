83c7d87eabd6da535912e5f93eee27da1db5021f
KevinWJohnson <kevin.johnson3@cox.net>
2018-08-16 11:38:25 -0700

    Initial commit of classroom

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Classroom from './component/Classroom/Classroom';
+
+class App extends Component {
+  render() {
+    return (
+      <Classroom />
+    );
+  }
+}
+
+export default App;
---
+++ src/component/Student/Student.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Student = (props) => (
+
+    <div className="student">
+        <p>Id: {props.id}</p>
+        <p>Name: {props.name}</p>
+        <p>Favorite Subject: {props.favoriteSubject}</p>
+        <button onClick={() => props.rollCall(props.name, props.favoriteSubject)}> Call on a student</button>
+    </div>
+
+
+);
+
+export default Student;
